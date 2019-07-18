ceaa8b7a1c8293c5db8032fca4a3311b10f985d4
MrRamsten <Levent.ramsten@gmail.com>
2019-07-18 20:56:14 +0200

    Testing Javascript
    Learning javascript

---
+++ script.js
@@ -0,0 +1,52 @@
+/*************************************
+ * Varibale and data types
+ */
+/*
+ var firstName = 'John';
+console.log(firstName); 
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// Detta är varablerna reglerna
+var _3years = 3;
+var johnMark ='John and MArk';
+var if = 23;
+*/
+
+
+/*************************************
+ * Varibale and data types
+ */
+
+
+
+
+ var firstName ='John';
+ var age = 28;
+// Type coercion
+console.log(firstName + ' ' + age);
+
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age +  ' year old' + job + '. Is he married? ' +  isMarried);
+
+// Veriable mutation
+
+
+
+
+
+
